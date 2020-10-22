49919ac6a11ca08d8061ad9cfc45a1064ae43c3b
Kaikai8888 <wuky812@gmail.com>
2020-10-22 23:53:28 +0800

    feat: modify shortenURL redirect code

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -8,12 +8,10 @@ const DOMAIN = 'http://localhost:3000/'
 router.get('/', (req, res) => res.render('index'))
 
 router.get('/:short', (req, res) => {
-  console.log('hi')
   const shortenURL = DOMAIN + req.params.short
   Url.find({ shortenURL })
     .then(urls => {
       if (urls.length === 0) return res.send('url not found')
-      console.log(res.redirect(urls[0].originalURL))
       res.redirect(urls[0].originalURL)
     })
     .catch(error => console.error(error))
