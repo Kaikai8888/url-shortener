efbeac0289526dc9d6f3bac14c574d0e307bb4d0
Kaikai8888 <wuky812@gmail.com>
2020-10-22 19:26:14 +0800

    feat: add index page

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 
+const router = require('./routes')
+
 const app = express()
 const PORT = 3000
 const URL = 'http://localhost'
@@ -9,8 +11,6 @@ const URL = 'http://localhost'
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
-app.get('/', (req, res) => {
-  res.send('Hi')
-})
+app.use(router)
 
 app.listen(PORT, () => console.log(`App is listening on ${URL}:${PORT}`))
