f79436940c43b48033786e97e5e9506e3f8a3b57
Kaikai8888 <wuky812@gmail.com>
2020-10-23 16:29:21 +0800

    feat: modify validator rule accept url without http or https

--- models/url.js
+++ models/url.js
@@ -3,7 +3,7 @@ const urlSchema = new mongoose.Schema({
   originalURL: {
     type: String,
     required: true,
-    match: [/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/, 'Invalid URL']
+    match: [/(https?:\/\/)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/, 'Invalid URL']
   },
   shortenURL: {
     type: String,
